4ca3a9c0e99fcf97cfd6cbec25a252678559156a
kimiko-i <73594659+kimiko-i@users.noreply.github.com>
2020-11-24 17:04:24 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show,]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,9 +23,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to "/items/#{params[:id]}"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :price, :detail, :category_id, :status_id, :shipping_id, :shipment_day_id, :send_from_id, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end  
+
 end
